5a5372f0e4d056aa4122de7c9892c07082286690
Thomas <tstopak@capital.edu>
2018-01-04 22:11:27 -0500

    Added minimal search to backend

--- B3_admin.php
+++ B3_admin.php
@@ -1,8 +1,15 @@
+<!--Make Compatible with fusion builder-->
+<style>
+  .fusion-builder-update-buttons{
+    display: none; 
+  }
+</style>
 
 <?php
 
 //HTML output for Admin Menu
 function b3_menu_html(){
+
   //Create a Query to pull up all posts with business post type
   $menu_query = new WP_Query(array('post_type'=>'business'));
   // Create a variable to refer to the post we just got with that query
@@ -18,7 +25,6 @@ function b3_menu_html(){
     ?>
     <div class="b3menuwrapper">
     <h1> B3 Directory settings </h1><br><br>
-
     <h2> Your Business Tags </h2>
     <?php
       echo '<ul>';
@@ -28,12 +34,25 @@ function b3_menu_html(){
       echo '</ul>';
     ?>
     <h2> Your Business Categories </h2><br><br>
-    <h2> Search Your Businesses </h2><br><br>
-    <form action="<?php echo admin_url('admin-post.php') ?>" method="get">
-    <input type="hidden" name="action" value="b3_admin_search">
-    <input type="text" name="query" value="Your Search Query">
+    <h2> Search Your Businesses </h2><br>
+    <form action="" method="POST">
+    <input type="text" name="query" placeholder="Your Search Query">
     <input type="submit" value="Submit">
     </form>
     <?php
+    if(!empty($_POST)){
+        wp_reset_postdata();
+        $businesslist  = new WP_Query(array("post_type"=>"business","title"=>$_POST[query]));
+        if($businesslist->have_posts()){
+        while($businesslist->have_posts()){
+          $businesslist->the_post();
+          echo "<h1>" . get_the_title() . "</h1><br>";
+        }
+      }else{
+        echo "<h2> No Businesses Found </h2>";
+      }
+    }
 }
+
+
 ?>
