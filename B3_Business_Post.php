8135d31dd17f5b1b8fd721258224389e3d042cc0
Thomas Stopak <tstopak@capital.edu>
2018-02-13 23:29:21 -0500

    Completely working with new file format

--- B3_Business_Post.php
+++ B3_Business_Post.php
@@ -8,94 +8,146 @@ require_once __DIR__ . '/B3_Refactor.php';
     private $city = '';
     private $state = '';
     private $sma = '';
-    private $cats = array();
-    private $tags = array();
+    private $cats = NULL;
+    private $tags = NULL;
     private $about ='';
     private $notes ='';
     private $burl ='';
     public function get_cats(){
-      return $this->$cats;
+      return $this->cats;
     }
     public function get_tags(){
-      return $this->$tags;
+      return $this->tags;
     }
     public function get_name(){
-      return $this->$name;
+      return $this->name;
     }
     public function get_notes(){
-      return $this->$notes;
+      return $this->notes;
     }
     public function get_url(){
-      return $this->$burl;
+      return $this->burl;
     }
     public function get_about(){
-      return $this->$about;
+      return $this->about;
     }
     public function get_city(){
-      return $this->$city;
+      return $this->city;
     }
     public function get_state(){
-      return $this->$state;
+      return $this->state;
     }
     public function get_sma(){
-      return $this->$sma;
+      return $this->sma;
     }
     public function get_content(){
 
       $content = '<p class="B3_City"><div class="B3_City_Label"> City:</div> ';
-      $content .= $this->$city;
+      echo "In content city = " . $this->city;
+      $content .= $this->city;
       $content .= '</p>';
       $content .= '<p class="B3_State"><div class="B3_State_Label"> State:</div> ';
-      $content .= $this->$state;
+      $content .= $this->state;
       $content .= '</p>';
       $content .= '<p class="B3_SMA"><div class="B3_SMA_Label"> Products Made In the USA:</div> ';
-      $content .= $this->$sma;
+      $content .= $this->sma;
       $content .= '</p>';
       $content .= '<p class="B3_AboutUs"><div class="B3_About_Label"> About Us:</div> ';
-      $content .= $this->$about;
+      $content .= $this->about;
       $content .= '</p>';
-      $content .= '<p class="B3_Notes"><div class="B3_Notes_Label"> Notes:</div> ';
-      $content .= $this->$notes;
-      $content .= '</p>';
-      $content .= '<p class="B3_URL"><div class="B3_URL_Label"> URL:</div> <a href=\''.$this->$url.'\'>';
-      $content .= $this->$url;
+      $content .= '<p class="B3_URL"><div class="B3_URL_Label"> URL:</div> <a href=\''.$this->burl.'\'>';
+      $content .= $this->burl;
       $content .= '</a></p>';
+      return $content;
 
     }
     public function add_cats($cat){
-      array_push($this->$cats, $cat);
+      if($this->cats == NULL){
+        $this->cats = array((int)get_cat_ID($cat));
+      }else{
+       $this->cats[]=(int)get_cat_ID($cat);
+     }
     }
     public function add_tags($tag){
-      array_push($this->$tags, $tag);
+      if($this->tags == NULL){
+        $this->tags = array($tag);
+      }else{
+      $this->tags[]=$tag;
+      }
     }
     public function set_name($newname){
-      $this->$name = $newname;
+      $this->name = $newname;
     }
     public function set_notes($newnotes){
-      $this->$notes = $newnotes;
+      $this->notes = $newnotes;
     }
     public function set_url($newurl){
-      $this->$burl = $newurl;
+      $this->burl = $newurl;
     }
     public function set_about($newabout){
-      $this->$about = $newabout;
+      $this->about = $newabout;
     }
     public function set_city($newcity){
-      $this->$city = $newcity;
+      $this->city = $newcity;
     }
     public function set_sma($sma_val){
-      $this->$sma = $sma_val;
+      $this->sma = $sma_val;
+    }
+    public function set_state($newstate){
+      $this->state = $newstate;
     }
     public function add_update(){
-      if(post_exists($this->$name)){
-        if(post_exists($this->$name, $this->get_content())){
-          $thispostobject = get_page_by_title( $this->$name, 'OBJECT', 'business');
-          $thispostid = $thispostobject->$ID;
+      if(post_exists($this->name)){
+        $thispostobject = get_page_by_title( $this->name, 'OBJECT', 'business');
+        $thispostid = $thispostobject->ID;
+        if(post_exists($this->name, $this->get_content())){
+          wp_set_post_categories( $thispostid, $this->get_cats());
+          wp_set_post_tags($thispostid, $this->get_tags());
+          echo "Update Successful";
+        }
+        else{
+          echo "updating";
+          $postupdatearray = array(
+            'ID'=> $thispostid,
+            'post_title'   => $this->get_name(),
+            'post_content' => $this->get_content(),
+          );
+          if($this->cats){
+            $postupdatearray['post_category'] = $this->get_cats();
+          }
+          if($this->tags){
+            $postupdatearray['tags_input'] = $this->get_tags();
+          }
 
-        }else{
-
+          if(!is_wp_error(wp_update_post($postupdatearray))){
+            echo "Update Successful";
+          }
+          else{
+            echo "Update Failed";
+          }
+        }
+      }else{
+        echo "<pre>";
+        var_dump($this);
+        $postinsertarray = array(
+          'post_title'   => $this->get_name(),
+          'post_content' => $this->get_content(),
+          'post_status' => self::$post_status,
+          'post_type' => self::$post_type,
+        );
+        if($this->cats){
+          echo "here 1997";
+          $postinsertarray['post_category'] = $this->get_cats();
+        }
+        if($this->tags){
+          $postinsertarray['tags_input'] = $this->get_tags();
+        }
+        $thisupload = wp_insert_post($postinsertarray);
+        if(is_wp_error($thisupload)){
+          die($thisupload->get_error_message());
         }
       }
     }
+
   }
 ?>
--- B3_Parser.php
+++ B3_Parser.php
@@ -9,22 +9,29 @@ function read_upload()
     $filearray  = explode("|", $filestring);
     //Create objects out of parsed data
     for ($row = 12; $row<count($filearray); $row=$row+12) {
+        $thisbusiness = new Business_Post();
         // get business information from the file array
         $bname = $filearray[$row];
         $bname = wp_strip_all_tags(htmlspecialchars(trim($bname),ENT_QUOTES));
         echo "Business Name: $bname <br>";
+        $thisbusiness->set_name($bname);
         $bcity = $filearray[$row+1];
         $bcity = wp_strip_all_tags(htmlspecialchars(trim($bcity),ENT_QUOTES));
         echo "Business City: $bcity <br>";
+        $thisbusiness->set_city($bcity);
+        $thisbusiness->add_tags($bcity);
         $bstate = $filearray[$row+2];
         $bstate = wp_strip_all_tags(htmlspecialchars(trim($bstate),ENT_QUOTES));
         echo "Business State: $bstate <br>";
+        $thisbusiness->set_state($bstate);
+        $thisbusiness->add_tags($bstate);
         $babout = $filearray[$row+3];
         $babout = wp_strip_all_tags(htmlspecialchars(trim($babout),ENT_QUOTES));
         echo "About Us: $babout <br>";
+        $thisbusiness->set_about($babout);
         $bcat1 = $filearray[$row+4];
         if ($bcat1!="") {
-          $bcat2 = wp_strip_all_tags(htmlspecialchars(trim($bcat2),ENT_QUOTES));
+          $bcat1 = wp_strip_all_tags(htmlspecialchars(trim($bcat1),ENT_QUOTES));
             echo "Business Category 1: $bcat1 <br>";
             if (get_cat_ID($bcat1)==0) {
                 echo "Category 1 not found <br>";
@@ -32,6 +39,7 @@ function read_upload()
                     echo "Category $bcat1 Successfully Created <br>";
                 }
             }
+            $thisbusiness->add_cats($bcat1);
         }
         $bcat2 = $filearray[$row+5];
         if ($bcat2!="") {
@@ -43,6 +51,7 @@ function read_upload()
                     echo "Category $bcat2 Successfully Created <br>";
                 }
             }
+            $thisbusiness->add_cats($bcat2);
         }
         $bcat3 = $filearray[$row+6];
         $bcat3 = wp_strip_all_tags(htmlspecialchars(trim($bcat3),ENT_QUOTES));
@@ -54,6 +63,7 @@ function read_upload()
                     echo "Category $bcat3 Successfully Created <br>";
                 }
             }
+            $thisbusiness->add_cats($bcat3);
         }
         $bcat4 = $filearray[$row+7];
         if ($bcat4!="") {
@@ -63,8 +73,10 @@ function read_upload()
                 echo "Category 4 not found <br>";
                 if (wp_create_category($bcat4) !=0) {
                     echo "Category $bcat4 Successfully Created <br>";
+                    $thisbusiness->add_cats($bcat4);
                 }
             }
+            $thisbusiness->add_cats($bcat4);
         }
         $bcat5 = $filearray[$row+8];
         if ($bcat5!="") {
@@ -76,6 +88,7 @@ function read_upload()
                     echo "Category $bcat5 Successfully Created <br>";
                 }
             }
+            $thisbusiness->add_cats($bcat5);
         }
         $bcat6 = $filearray[$row+9];
         if ($bcat6!="") {
@@ -87,31 +100,20 @@ function read_upload()
                     echo "Category $bcat6 Successfully Created <br>";
                 }
             }
+            $thisbusiness->add_cats($bcat6);
         }
         $btag1 = $filearray[$row+10];
         $btag1 = wp_strip_all_tags(htmlspecialchars(trim($btag1),ENT_QUOTES));
         echo "Products made in USA: $btag1 <br>";
+        $thisbusiness->add_tags($btag1);
+        $thisbusiness->set_sma($btag1);
         $burl = $filearray[$row+11];
         $burl= wp_strip_all_tags(htmlspecialchars(trim($burl)));
         echo "URL: <a href='http://$burl'> $burl </a> <br>";
+        $thisbusiness->set_url($burl);
         echo "<br>";
         echo "<br>";
-        $categoryid = array();
-        for ($k = 1; $k<7; $k++) {
-            if (get_cat_ID(${"bcat" . $k})) {
-                $categoryid[$k-1]=get_cat_ID(${"bcat" . $k});
-            }
-        }
-
-        $new_business_post=array(
-          'post_title'    => wp_strip_all_tags(htmlspecialchars(trim($bname),ENT_QUOTES)),
-          'post_content'  => $babout . "<br>" . $burl . "<br>" . $bnotes,
-          'post_status'   => 'publish',
-          'post_type'     => 'business',
-          'post_category' => $categoryid,
-          'tags_input' => $btag1
-          );
-          wp_insert_post($new_business_post);
-    }
+        $thisbusiness->add_update();
+      }
 }
 ?>
